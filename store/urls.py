f5ae4a5032a4f0b22d705a5d1774ca999f0f64ae
ksenieuphoria <ksushaspring@gmail.com>
2023-05-09 23:19:39 +0300

    upd

--- store/urls.py
+++ store/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
 
     # Маршрут "Главная страница"
     path('', views.main, name='main'),
-    # Маршрут "Список всех продуктов"
-    path('store', views.store, name='store'),
     # Маршрут "Детали продукта"
     path('product/<int:product_id>', views.product_detail, name='product_detail'),
+    # Маршрут "Список всех продуктов"
+    path('store', views.store, name='store'),
     # Маршрут "О компании"
     path('about', views.about, name='about'),
 
